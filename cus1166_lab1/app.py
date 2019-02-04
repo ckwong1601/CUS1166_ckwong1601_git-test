3a579600e211bb544627bc9414c30834a84a4dfa
Calvin Kwong <calvin.kwong17@stjohns.edu>
2019-02-04 16:14:06 -0500

    Student roster created without appending

--- cus1166_lab1/app.py
+++ cus1166_lab1/app.py
@@ -4,16 +4,18 @@ from mymodules.models import Student
 def main():
     # create a roster of 10 students, print out
     # list of students, and print average score
-    student_roster = [Student("Vinay Woodley", 67)]
-    student_roster.append(Student("Abigail Monaghan", 76))
-    student_roster.append(Student("Menaal Johnston", 86))
-    student_roster.append(Student("Darrel Wooten", 91))
-    student_roster.append(Student("Danny Rooney", 69))
-    student_roster.append(Student("Ariel Whitfield", 73))
-    student_roster.append(Student("Emman Sanders", 88))
-    student_roster.append(Student("Milla Diaz", 97))
-    student_roster.append(Student("Eileen Landry", 56))
-    student_roster.append(Student("Joesph Guasp", 75))
+    student_roster = [
+        Student("Vinay Woodley", 67),
+        Student("Abigail Monaghan", 76),
+        Student("Menaal Johnston", 86),
+        Student("Darrel Wooten", 91),
+        Student("Danny Rooney", 69),
+        Student("Ariel Whitfield", 73),
+        Student("Emman Sanders", 88),
+        Student("Milla Diaz", 97),
+        Student("Eileen Landry", 56),
+        Student("Joesph Guasp", 75)
+        ]
 
     print("Student roster:")
     for student in student_roster:
